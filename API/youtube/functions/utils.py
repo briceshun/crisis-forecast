14a3bb788c6d451b00d4ea5596cd213a4ed1c206
briceshun <bricevalentinshun@gmail.com>
2022-09-20 10:07:39 +0000

    modified for commentThreads

--- API/youtube/functions/utils.py
+++ API/youtube/functions/utils.py
@@ -1,15 +1,37 @@
 # Clean up function
 def cleanUp(data):
     output = []
-    # Loop over all items in response
     keys = list(data['items'][0].keys())
-    # Create dict if id not a dict
-    try:
+    keys2 = list(data['items'][0][keys[1]].keys())
+
+    # Check if CommentThreads API
+    if len(keys2) == 1:
+        ## CommentThreads API
+        keys3 = list(data['items'][0]['snippet']['topLevelComment'].keys())
+        ### Loop over all items in response
         for item in data['items']:
-            dict = item[keys[0]] | item[keys[1]]
-            output.append(dict)
-    except TypeError:
-        for item in data['items']:
-            dict = {keys[0]: item[keys[0]]} | item[keys[1]]
-            output.append(dict)
+            #### Create variables to store data
+            dict = item['snippet']['topLevelComment']
+            #### Extract snippet dict to combine
+            snippet = dict['snippet']
+            #### Loop over all keys other than Snippet
+            for i in keys3[1:]:
+                snippet |= {i: dict[i]}
+            #### Add replies
+            try:
+                snippet |= dict['replies']
+            except:
+                pass
+            output.append(snippet)
+
+    # Search and Videos API
+    else:
+        try:
+            ## Loop over all items in response of Search API
+            for item in data['items']:
+                output.append(item[keys[0]] | item[keys[1]])
+        except TypeError:
+            ## Create dict for each item and append other dict for Videos API
+            for item in data['items']:
+                output.append({keys[0]: item[keys[0]]} | item[keys[1]])
     return output
